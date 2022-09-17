7b38cec751f82162dd33b18664af866a05f52489
unknown <gulidin25@gmail.com>
2022-09-17 20:51:39 +0300

    Первая задача

---
+++ 1/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+/*   a = 5; b = 7 -> max = 7
+     a = 2 b = 10 -> max = 10
+     a = -9 b = -3 -> max = -3    */
+
+Console.WriteLine("Введите первое целое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе целое число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    Console.Write($"Max ={a}");
+}
+else if (a < b)
+{
+    Console.Write($"Max ={b}");
+}
+else if (a == b)
+{
+    Console.Write("Оба значения равны");
+}
+else
+{
+    Console.Write("Условие введено не корректно!");
+}
